f8330733ffa936a75680146e55a19a5997e2a15e
Tyreke Hetzel <99774751+Hakka2424@users.noreply.github.com>
2024-11-16 21:24:28 -0700

    Server.h
    Finish Server.h with comments

--- Server.h
+++ Server.h
@@ -1,30 +1,34 @@
-// Server.h 
-// Name: Desiree Gutierrez and Tyreke
-//Class: CS484 Computer Networking
+// Server.h
+// Names: Desiree Gutierrez and Tyreke Hetzel
+// Class: CS484 Computer Networking
 // Date: 11/6/2024
+// Purpose: This header file defines the structure and function prototypes for the Server used in a multi-threaded HTTP server. 
+// It includes the `Server` struct definition and the prototype for the server constructor function, which initializes the server with specified parameters for domain, service, protocol, interface, and backlog.
 
 #ifndef Server_h
 #define Server_h
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdint.h>
 
-struct Server{
-    int domain;
-    int service;
-    int protocol;
-    long interface;
-    int backlog;
+// Structure to represent the Server
+struct Server {
+    int domain;               // Address family (AF_INET for IPv4)
+    int service;              // Socket type (SOCK_STREAM for TCP)
+    int protocol;             // Protocol to be used (IPPROTO_TCP)
+    uint32_t interface;       // Network interface (INADDR_ANY for all available interfaces)
+    int backlog;              // Maximum number of pending connections in the queue
 
-    struct sockaddr_in address;
-    int addrlen;
-    int socket;
+    struct sockaddr_in address; // Server address structure
+    int addrlen;                // Length of the address structure
+    int socket;                 // Socket file descriptor
 
-    void (*launch) (struct Server *server);
+    // Function pointer for launching the server (not used in the current implementation)
+    void (*launch)(struct Server *server);
 };
 
+// Constructs a server with the specified domain, service, protocol, interface, and backlog.
+struct Server server_constructor(int domain, int service, int protocol, uint32_t interface, int backlog);
 
-struct Server server_constructor(int domain, int service, int protocol,
- long interface, int backlog);
-
-#endif /*end of server.h*/
+#endif /* end of Server_h */
